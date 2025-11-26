59e69754d45975122ccb2b0f8cfb0a160e7d858a
martinehagberg <martine.hagberg@gmail.com>
2025-11-26 15:05:08 +0100

    Laget finance i models

---
+++ SportsWorldAPI/Models/Finance.cs
@@ -0,0 +1,10 @@
+namespace SportsWorldAPI.Models {
+    public class Finance
+    {
+        public int Id {get; set;}
+        public double MoneyLeft {get; set;}
+        public int NumberOfPurchases { get; set;}
+        public double MoneySpent { get; set;}
+        
+    }
+}
