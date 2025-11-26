df01cd77a788e6f83f21b45e2311a58d830c895c
siljerognstad <siljerognstad06@outlook.com>
2025-11-26 17:05:21 +0100

    venue lagt til

---
+++ SportsWorldAPI/Models/Venue.cs
@@ -0,0 +1,8 @@
+namespace SportsWorldAPI.Models {
+    public class Venue {
+        public int Id { get; set ;}
+        public string Name { get; set; } = "";
+        public int Capacity { get; set; }
+        public string Image { get; set; } = "";
+    }
+}
