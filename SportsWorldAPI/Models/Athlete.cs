7f4a529dbc2b007e0775a3cf81272d2acab41462
paulabv04 <paulavega897@gmail.com>
2025-11-26 14:27:47 +0100

    Mappstruktur

---
+++ SportsWorldAPI/Models/Athlete.cs
@@ -0,0 +1,9 @@
+namespace SportsWorldAPI.Models
+{
+    public int Id{get; set;}
+    public string Name {get; set;} = "";
+    public string Gender {get; set;} = "";
+    public string Price {get; set;}
+    public string Image {get; set;} = "";
+    public string PurchaseStatus {get; set;}
+}
