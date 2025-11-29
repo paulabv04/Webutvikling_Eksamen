37672cc441f0606cd997300d920a48da37534465
paulabv04 <paulavega897@gmail.com>
2025-11-29 18:37:34 +0100

    AthletesPage og et par andre endringer

--- SportsWorld-App/src/interfaces/IAthletes.ts
+++
@@ -1,9 +0,0 @@
-
-export interface IAthlete {
-    id: number;
-    name: string;
-    gender: string;
-    price: number;
-    image: string;
-    purchaseStatus: boolean;
-}
--- SportsWorld-App/src/main.tsx
+++ SportsWorld-App/src/main.tsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { FinanceProvider } from './contexts/FinanceContext.tsx'
+import { BrowserRouter } from 'react-router-dom';
+import ReactDOM from "react-dom/client";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
@@ -11,3 +13,8 @@ createRoot(document.getElementById('root')!).render(
     </FinanceProvider>
   </StrictMode>,
 )
+ReactDOM.createRoot(document.getElementById('root')!).render(
+    <BrowserRouter>
+    <App />
+    </BrowserRouter>
+);
